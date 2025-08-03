ec65408cbc8c7a4346c038b0cc278ef3d923096e
Chris McDonough <chrism@plope.com>
2025-08-02 23:29:44 -0400

    build a derivation

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,14 @@
 { pkgs, lib, config, inputs, ... }:
 
+let
+  deriv = pkgs.writeShellScriptBin "testderiv" ''
+    echo yup
+  '';
+in
 {
+
+  packages = [ deriv ];
+
   scripts.hello.exec = ''
     echo hello
   '';
